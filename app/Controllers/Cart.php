2ed43529645187db68dbb9cbd20e98701c333d0b
UmmuIzzatulWidyatama <ummu.izzatul@widyatama.ac.id>
2024-09-21 14:42:44 +0700

    version 1

---
+++ app/Controllers/Cart.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controllers;
+
+class Cart extends BaseController
+{
+    public function index()
+    {
+        echo view('part/header');
+        echo view('part/topbar');
+        echo view('part/navbar');
+        echo view('cart');
+        echo view('part/footer');
+    }
+}
